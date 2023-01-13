87ffffe09d385e1ef06f8c5141c2ed1aa617d414
Michel <michel@famille-eisenmann.net>
2023-01-13 18:39:31 +0100

    Transform accents into html special chars (for titles)

--- htdocs/public/galerie_spread.php
+++ htdocs/public/galerie_spread.php
@@ -56,12 +56,12 @@ foreach( $dico->mostRecents as $paint ) {
   <div class="w3-card-4">
        <a target="_blank" href="images/<?= $paint->file; ?>">
             <img src="images/<?= $paint->file; ?>"
-                 alt="<?= $paint->title; ?>"
+                 alt="<?= htmlspecialchars($paint->title); ?>"
 		 style="width:100%"
 	    >
        </a>
        <div class="w3-container w3-center">
-         <p><?= ucfirst($paint->title); ?></p>
+          <p><?= ucfirst(htmlspecialchars($paint->title)); ?></p>
        </div>
   </div>
 </div>
