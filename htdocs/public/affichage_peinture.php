4a5744c26d05a341ecdca26db5690b5ccbb6e93f
Michel <michel@famille-eisenmann.net>
2024-02-20 19:08:55 +0100

    Restrict size of paint on computer so that it does zoom too much

--- htdocs/public/affichage_peinture.php
+++ htdocs/public/affichage_peinture.php
@@ -25,22 +25,39 @@
  $prev= $dico->get_prev($rank);
 ?>
 
-    <title><?= ucfirst(htmlspecialchars($paint->full_title())); ?> </title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<title><?= ucfirst(htmlspecialchars($paint->full_title())); ?> </title>
+<meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+          
+          <style>
+.top-container {
+    padding-top: 10px;
+}
+.title {
+	font-size: 12px; 
+}
+.limited {
+    max-height: 800px;
+    max-width: 900px;
+     display: block;
+     margin-left: auto;
+     margin-right: auto;
+}
 
-    <style>
-	.top-container {
-      padding-top: 10px;
-      }
-        .title {
-	        font-size: 12px; 
-      }
-    </style>
-  </head>
+
+/* Adjustments for phones */
+@media only screen and (max-device-width: 576px) {
+.limited {
+    width: 100%;
+}
+}
+
+          </style>
+
+        </head>
 
   <body>
 <!-- Navbar (sit on top) -->
@@ -103,7 +120,7 @@
     
   <div class="w3-container">
     <div class="w3-card-4" >
-      <img src="images/<?= $paint->file; ?>" style="width:100%"/>
+      <img src="images/<?= $paint->file; ?>" class="limited" style=""/>
     </div>
   </div>
 
