2289a7ede35e1288c13b6839bbdd46559e4fb598
gisele eisenmann <gisele.eisenmann@gmail.com>
2026-08-17 17:35:54 +0200

    copyright: ouvrir un nouvel onglet art majeur, linkedin...

--- htdocs/public/copyright.php
+++ htdocs/public/copyright.php
@@ -9,13 +9,13 @@
   <a href="<?= Translator::url('/public/exposition_suivante_flyer.php') ?>"><?= Translator::t('couleursetmodele') ?></a>
   <p>
     <div class="">
-      <a class="gem-a" href="https://www.instagram.com/gem.eisenmann/">
+      <a target="_blank" class="gem-a" href="https://www.instagram.com/gem.eisenmann/">
 	<i class="fa fa-instagram fa-fw"></i>
       </a>
-      <a class="gem-a" href="https://www.artmajeur.com/gisele-eisenmann-montagne">
+      <a target="_blank" class="gem-a" href="https://www.artmajeur.com/gisele-eisenmann-montagne" >
         <img src="/public/images/web/artmajeur-com.png" class="ArtMajeur-fixed-size" alt="Art Majeur">		
       </a>
-	<a class="gem-a" href="https://www.linkedin.com/in/gisele-m-42918b42/">
+	<a target="_blank" class="gem-a" href="https://www.linkedin.com/in/gisele-m-42918b42/ target=_BLANK">
 	<i class="fa fa-linkedin fa-fw"></i>
       </a>
     </div>
