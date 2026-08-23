1cb5b08b2df19e8936e95789bc8c94d9c2911c8c
Michel <michel@famille-eisenmann.net>
2026-08-23 14:24:00 +0200

    One can click on is-situ images to see them

--- htdocs/private/column_generator.php
+++ htdocs/private/column_generator.php
@@ -66,27 +66,31 @@ class ColumnGenerator {
   }
   
   // Use this function whent the image you want to display is not a paint (for ex in-situ photo)
+  // One can click on the image to have a modal view of the image
   function generate_part_not_a_paint( $paint, $tagname, $height_style, $indent) {
-    $rank= $this->serie_dico->get_id_rank($paint->id);
-    $serie= $this->serie_dico->name;
-    $url= Translator::url('/public/contenu_d_une_serie.php?serie=' .$serie .'&rank=' .$rank);
     $file= "images/" .$paint->file;
     $alt= Translator::t($paint->getAltId());
+    $id= $paint->id;
+
+    $display_modal= "document.getElementById('" .$id ."').style.display='block'";
+    $hide_modal= "document.getElementById('" .$id ."').style.display='none'";
     
     $tt2ab= "    ";
     $tt3ab= $tt2ab .$tt2ab;
     $tt4ab= $tt3ab .$tt2ab;
     $tt5ab= $tt4ab .$tt2ab;
-    //    $this->print_line( $indent, "<a class=\"gem-a\" href=\"" .$url ."\">" );
-    $this->print_line( $indent, $tt2ab ."<div class=\"w3-display-container w3-padding" ." gem-" .$tagname ."\" >" );
-    $this->print_line( $indent, $tt3ab   ."<img src=\"" .$file ."\" class=\"" ."gem-" .$tagname ."\" alt=\"" .$alt ."\">" );
-    //    $this->print_line( $indent, $tt3ab     ."<div class=\"w3-display-middle w3-hide-small gem-hover\">" );
-    //    $this->print_line( $indent, $tt4ab       .Translator::t($paint->getTitleId()) ." </br> " .Translator::t($paint->type) ." </br> " .$paint->get_size() ." </br> ");
-    //    $this->print_line( $indent, $tt4ab       .Translator::t($paint->get_status()) ." </br> " );
-    //    $this->print_line( $indent, $tt3ab     ."</div>" );
+    $this->print_line( $indent, $tt2ab   ."<div class=\"w3-display-container w3-padding" ." gem-" .$tagname ."\" >" );
+    $this->print_line( $indent, $tt3ab     ."<img onclick=\"" .$display_modal ."\" src=\"" .$file ."\" class=\"" ."gem-" .$tagname ."\" alt=\"" .$alt ."\">" );
+    $this->print_line( $indent, $tt2ab   ."</div>" );
+
+    // This is the modal that will pop when one clicks on the image
+    $this->print_line( $indent, $tt2ab ."<div id=\"" .$id ."\" class=\"w3-modal\"" ."onclick=\"this.style.display='none'\"" ." >" );
+    $this->print_line( $indent, $tt2ab ."       <span class=\"w3-button w3-hover-red w3-xlarge w3-display-topright\">&times;</span> ");
+    $this->print_line( $indent, $tt2ab ."       <div class=\"w3-modal-content w3-animate-zoom\">" );
+    $this->print_line( $indent, $tt3ab ."            <img src=\"" .$file ."\" alt=\"" .$alt ."\" width=\"100%\" >" );
+    $this->print_line( $indent, $tt2ab ."       </div>" );
     $this->print_line( $indent, $tt2ab ."</div>" );
-    //    $this->print_line( $indent, "</a>" );
-  }
+   }
   
   function generate_style( $id, $color ) {
     $paint= $this->paints[$id];
