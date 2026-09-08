7864fd99eb4d212c28dbb13bd926a8bd5eb34f97
Michel <michel@famille-eisenmann.net>
2026-09-08 17:24:49 +0200

    Inserted image in text + modal display

--- htdocs/public/demarche.php
+++ htdocs/public/demarche.php
@@ -25,6 +25,16 @@
   <link rel="stylesheet" href="./serie-style.css">
 
   <style>
+.img-left {
+    float: left;
+    margin-right: 15px;
+    width: 100px;
+}
+.img-right {
+    float: right;
+    margin-right: 15px;
+    width: 100px;
+}
   </style>
   
   <body>
@@ -36,15 +46,50 @@
 
       <!-- photo -->
       <div class="w3-content w3-container">
-	<a href="<?= Translator::url('/index.php') ?>">
-	  <img class="artiste to-be-signed" src="../public/images/Acrylique/20260125_PurpleSeagull_AC73x50.jpg" alt="Purple seagull" /></a>
       </div>
       
       <!-- text -->
       <div class="w3-content w3-container gem-justify">
-	<?= Translator::t('DemarchePrologue'); ?>
-	<?= Translator::t('DemarcheTexte'); ?>	  
+
+	          <img class="artiste to-be-signed img-right"
+                       src="../public/images/Acrylique/20260125_PurpleSeagull_AC73x50_small.jpg"
+                       alt="Purple seagull"
+                       onclick="document.getElementById('ModalSeagull').style.display='block'"
+                       />
+
+               <!-- the part that is displayed when one clicks on the above image -->
+               <div id="ModalSeagull" class="w3-modal" onclick="this.style.display='none'">
+                 <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+                 <div class="w3-modal-content w3-animate-zoom">
+	               <img class="artiste to-be-signed"
+                            src="../public/images/Acrylique/20260125_PurpleSeagull_AC73x50.jpg"
+                            alt="Purple seagull" width="100%"/>
+                 </div>
+               </div>
+
+	           <?= Translator::t('DemarchePrologue'); ?>
       </div>
+
+      <div class="w3-content w3-container gem-justify">
+        <img class="artiste to-be-signed img-left"
+             src="../public/images/Acrylique/20260615_Mimosa_AC27x19_small.jpg"
+             alt="Mimosa"
+             onclick="document.getElementById('ModalMimosa').style.display='block'"
+             />
+
+               <!-- the part that is displayed when one clicks on the above image -->
+               <div id="ModalMimosa" class="w3-modal" onclick="this.style.display='none'">
+                 <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+                 <div class="w3-modal-content w3-animate-zoom">
+	               <img class="artiste to-be-signed"
+                            src="../public/images/Acrylique//20260615_Mimosa_AC27x19.jpg"
+                            alt="Mimosa" width="100%"/>
+                 </div>
+               </div>
+
+        <?= Translator::t('DemarcheTexte'); ?>	  
+      </div>
+
       
       <!-- Footer -->
       <?php include("../public/copyright.php"); ?>
